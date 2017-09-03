6dd674bf31f2c000f8009fb2186ee72b9bc34a8f
CUTYlife <2055305346@qq.com>
2017-09-03 19:02:27 +0800

    22222222

--- app/src/main/java/com/sixin/doublelist/MainActivity.java
+++ app/src/main/java/com/sixin/doublelist/MainActivity.java
@@ -3,17 +3,22 @@ package com.sixin.doublelist;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
-import com.sixin.doublelist.view.HorizontalListView;
 import com.sixin.doublelist.view.HAdapter;
+import com.sixin.doublelist.view.SXTree;
+import com.sixin.doublelist.view.VAdapter;
+
+import db.DBHelper;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = MainActivity.class.getName();
 
-    private HorizontalListView mListView;
-    private HAdapter adapter;
-    private String[] leftStr = new String[]{"面食类", "盖饭", "寿司", "烧烤", "酒水", "凉菜", "小吃", "粥", "休闲"};
-    private boolean[] flagArray = {true, false, false, false, false, false, false, false, false};
+    private SXTree mSXTree;
+
+    private HAdapter mHAdapter;
+    private VAdapter mVAdapter;
+
+    private DBHelper dbHelper;
 
 
 
@@ -21,20 +26,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        initView();
-        String a = "merge";
-//        initView();
-        //programmer1
-        setListViewAdapter();
     }
 
-    private void setListViewAdapter() {
 
-    }
-
-    private void initView() {
-
-    }
 
 
 }
